d8d53653f948497131bd03443cca0ec9f63bcc40
eerolli <eero_liuska_8@hotmail.com>
2022-04-06 14:21:20 +0300

    Added simple operator functions to script

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(1, 2));
+
+function subtract(a, b){
+    return a - b;
+}
+
+console.log(subtract(2, 1));
+
+function multiply(a, b) {
+    return a * b;
+}
+
+console.log(multiply(3, 4));
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(divide(6, 2));
