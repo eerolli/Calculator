4a28abe17d42424d36ef73e697996ffbd9c83b63
eerolli <eero_liuska_8@hotmail.com>
2022-04-06 15:07:21 +0300

    Added buttons and layout

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ```
 The Odin Project challenge
 Created with html, css and javascript
+A simple calculator that can add, subtract, multiply and divide
 ```
--- script.js
+++ script.js
@@ -1,23 +1,31 @@
+
 function add(a, b) {
     return a + b;
 }
-
-console.log(add(1, 2));
 
 function subtract(a, b){
     return a - b;
 }
 
-console.log(subtract(2, 1));
-
 function multiply(a, b) {
     return a * b;
 }
-
-console.log(multiply(3, 4));
 
 function divide(a, b) {
     return a / b;
 }
 
-console.log(divide(6, 2));
+function operate(operator, a, b){
+    if(operator === "+"){
+        add(a, b);
+    }
+    else if(operator === "-"){
+        subtract(a, b);
+    }
+    else if(operator === "*"){
+        multiply(a, b);
+    }
+    else if(operator === "/"){
+        divide(a, b);
+    }
+}
