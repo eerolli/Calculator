148cdf65d3cd5836d9365aae7e7ad16d501d3b18
eerolli <eero_liuska_8@hotmail.com>
2022-04-07 22:40:52 +0300

    Added delete button, changed styles a bit, added variables for buttons

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const numberBtns = document.querySelectorAll("[data-number]");
+const operationBtns = document.querySelectorAll("[data-operation]");
+const equalsBtn = document.querySelector("[data-equals]");
+const clearBtn = document.querySelector("[data-clear]");
+const deleteBtn = document.querySelector("[data-delete]");
+const previous = document.querySelector("[data-previous]");
+const current = document.querySelector("[data-current]");
+
 
 function add(a, b) {
     return a + b;
